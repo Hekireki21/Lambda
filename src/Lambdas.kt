eaac6f270163c697f8163fb3dfd5925dd58e6e68
Hekireki21 <53225522+Hekireki21@users.noreply.github.com>
2019-08-05 21:58:22 -0500

    L

---
+++ src/Lambdas.kt
@@ -0,0 +1,34 @@
+fun convert(x: Double,
+            converter: (Double) -> Double) : Double {
+    val result = converter(x)
+    println("$x is converted to $result")
+    return result
+}
+
+
+fun convertFive(converter: (Int) -> Double) : Double {
+    val result = converter(5)
+    println("5 is converted to $result")
+    return result
+}
+
+fun main(args: Array<String>) {
+//    var addfive = { x: Int -> x + 5 }
+//    println("Pass 6 to addfive: ${addfive(6)}")
+//
+//    val addInts = { x: Int, y: Int -> x + y }
+//    val result = addInts.invoke(6,7)
+//    println("Pass 6, 7 to addInts: $result")
+//
+//    val intLambda: (Int, Int) -> Int = { x, y -> x * y }
+//    println("Pass 10, 12 to intLambda: ${intLambda(10, 11)}")
+//
+//    val addSeven: (Int) -> Int = { it + 7 }
+//    println("Pass 12 to addSeven: ${addSeven(12)}")
+//
+//    val myLambda: () -> Unit = { print("Hi!") }
+//    myLambda()
+
+    convert(20.0) { it * 1.8 + 32 }
+    convertFive { it * 1.8 + 32 }
+}
